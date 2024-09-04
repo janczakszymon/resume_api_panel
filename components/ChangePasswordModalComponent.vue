b3be289d29e401f2ea6a7bb524971de7c22088f0
szymon <s.janczak04@gmail.com>
2024-09-04 19:21:24 +0200

    finished change password feature

--- components/ChangePasswordModalComponent.vue
+++ components/ChangePasswordModalComponent.vue
@@ -1,23 +1,49 @@
 <template>
 	<UModal v-model="isOpen">
 		<UForm
-			:state="{}"
+			:state="passwordData"
+			:schema="schema"
 			class="space-y-4 p-3"
+			@submit="updatePassword"
 		>
-			<UFormGroup :label="$t('oldPassword')">
-				<UInput />
+			<UFormGroup
+				:label="$t('oldPassword')"
+				name="oldPassword"
+			>
+				<UInput
+					v-model="passwordData.oldPassword"
+					required
+				/>
 			</UFormGroup>
-			<UFormGroup :label="$t('newPassword')">
-				<UInput />
+			<UFormGroup
+				:label="$t('newPassword')"
+				name="newPassword"
+			>
+				<UInput
+					v-model="passwordData.newPassword"
+					required
+				/>
 			</UFormGroup>
-			<UFormGroup :label="$t('confirmNewPassword')">
-				<UInput />
+			<UFormGroup
+				:label="$t('confirmNewPassword')"
+				name="confirmNewPassword"
+			>
+				<UInput
+					v-model="passwordData.confirmNewPassword"
+					required
+				/>
 			</UFormGroup>
 			<div class="flex justify-end gap-3">
-				<UButton color="green">
+				<UButton
+					color="green"
+					type="submit"
+				>
 					{{ $t('save') }}
 				</UButton>
-				<UButton color="gray">
+				<UButton
+					color="gray"
+					@click="isOpen=false"
+				>
 					{{ $t('discard') }}
 				</UButton>
 			</div>
@@ -26,5 +52,36 @@
 </template>
 
 <script setup lang="ts">
-const isOpen = defineModel();
+import { z } from 'zod';
+
+const isOpen = defineModel<boolean>();
+
+const i18n = useI18n();
+const toast = useToast();
+const { data } = useAuthState();
+const passwordData = ref({
+	oldPassword: '',
+	newPassword: '',
+	confirmNewPassword: ''
+});
+
+const schema = z.object({
+	oldPassword: z.string().min(1, { message: i18n.t('fieldIsRequired') }),
+	newPassword: z.string().min(1, { message: i18n.t('fieldIsRequired') }),
+	confirmNewPassword: z.string().min(1, { message: i18n.t('fieldIsRequired') })
+});
+
+function updatePassword(): void {
+	useApi(`/users/${data.value?.id}/passwords/`, {
+		method: 'PUT',
+		body: JSON.stringify(passwordData.value)
+	})
+		.then(() => {
+			toast.add({ title: i18n.t('saved'), color: 'green' });
+			isOpen.value = false;
+		})
+		.catch(() => {
+			toast.add({ title: i18n.t('errorOccurred'), color: 'red' });
+		});
+}
 </script>
